5aecc08f22aad3fa1d30960c2797333dd9ab1129
Kevin Klions <kklions1@gmail.com>
2019-04-23 22:11:52 -0400

    finished through 18:00 in part 3

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+    // Max health
+    public float health = 20f;
+    float currentHealth;
+    public GameObject healthBarPrefab;
+    GameObject healthBar;
+
+    // How much the enemy is "worth" when destroyed
+    public float worth = 4f;
+
+    void Awake() { 
+        currentHealth = health;
+        // this may need some tweaking to make it look right
+        Vector3 healthBarOffset = new Vector3(0, 0, .25f);
+        healthBar = Instantiate(healthBarPrefab, transform.position + healthBarOffset, Quaternion.identity, transform);
+
+    }
+    void Update() {
+        
+    }
+
+    public void Hurt(float damage) { 
+        currentHealth -= damage;
+
+        // Is dead?
+        if (currentHealth <= 0) { 
+            Money.Amount += worth;
+            Destroy(gameObject);
+        } 
+        Transform piviot = healthBar.transform.Find("HeathlyPiviot");
+        Vector3 scale = piviot.localScale;
+        scale.x = Mathf.Clamp(currentHealth/health, 0, 1);
+        piviot.localScale = scale;
+    }
+}
---
+++ Assets/Scripts/Money.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Money {
+    public static float Amount = 100f;
+}
