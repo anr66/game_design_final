11d1c0bd4781b36432ca72f9cd0424e4d8f05474
unknown <anr66@zips.uakron.edu>
2019-04-27 21:13:52 -0400

    Add stuff so we dont break it

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour
+{
+    public Text moneyText;
+
+    public GameObject basicTowerPrefab;
+    GameObject boughtTower;
+
+    // Update is called once per frame
+    void Update()
+    {
+        moneyText.text = "Money: $" + Money.Amount;
+    }
+
+    public void BuyBasicTower()
+    {
+        // not enough money
+        if (Money.Amount < 40 || boughtTower != null)
+        {
+            return;
+        }
+
+        // spawn new tower
+        boughtTower = Instantiate(basicTowerPrefab, Camera.main.ScreenToWorldPoint(new Vector3(0.5f, 0.5f)), Quaternion.identity);
+
+        // make tower transparent
+        Color color = boughtTower.GetComponent<Renderer>().material.color;
+        color.a = 0.5f;
+        boughtTower.GetComponent<Renderer>().material.color = color;
+
+        // take away money
+        Money.Amount -= 40;
+    }
+
+}
